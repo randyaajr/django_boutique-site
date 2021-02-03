26a092b72f515226d345ce859392678185647fcb
randyaajr <randyaajrsp@gmail.com>
2021-02-03 23:17:34 +0000

    Added builtins for crispy-forms (tags)&(field)

--- boutiquesite/settings.py
+++ boutiquesite/settings.py
@@ -76,12 +76,17 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                # Required by allauth
+                # required by allauth
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
