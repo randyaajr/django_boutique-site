06a1d5b74055766ca1e74e4b8ef861ef20805f9a
randyaajr <randyaajrsp@gmail.com>
2021-02-04 19:49:41 +0000

    Added stripe to context

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_BupbWtI4ZQOacv8NRd4Spxz400WzoJUvyc',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
