f64bd875ebc5d38bb55e25a931a8bfd2dc946a7b
randyaajr <randyaajrsp@gmail.com>
2021-02-04 21:56:03 +0000

    Warning message added as was not saved last commit

--- checkout/views.py
+++ checkout/views.py
@@ -26,9 +26,12 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
