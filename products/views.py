ec2f3cc4ae4aefae579fbda7e7b6ef95c0069cbc
randyaajr <randyaajrsp@gmail.com>
2021-02-08 22:17:38 +0000

    Delete product view created

--- products/views.py
+++ products/views.py
@@ -42,11 +42,13 @@ def all_products(request):
             query = request.GET['q']
             if not query:
                 messages.error(
-                    request, "You didn't enter any search criteria!")
+                    request,
+                    "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
             queries = Q(
-                name__icontains=query) | Q(description__icontains=query)
+                name__icontains=query) | Q(
+                    description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -78,9 +80,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request,
@@ -120,3 +122,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
